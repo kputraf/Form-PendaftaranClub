7c0caf4f64b445c315785b32e29011866eeeb646
Krisna Putra F <contact.krisnapf@gmail.com>
2016-09-12 23:02:32 +0700

    tambah control, button blm fungsi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2013/formpendaftaranclub/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2013/formpendaftaranclub/MainActivity.java
@@ -2,19 +2,21 @@ package id.sch.smktelkom_mlg.tugas01.xirpl2013.formpendaftaranclub;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText etNama;
     CheckBox cbPG, cbSG, cbC;
-    RadioButton rbL, rbP;
+    RadioGroup rgStatus;
     Button buttonOK;
     Spinner spKota;
-
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +27,18 @@ public class MainActivity extends AppCompatActivity {
         cbPG = (CheckBox) findViewById(R.id.checkBoxPG);
         cbSG = (CheckBox) findViewById(R.id.checkBoxSG);
         cbC = (CheckBox) findViewById(R.id.checkBoxC);
-        rbL = (RadioButton) findViewById(R.id.radioButtonL);
-        rbP = (RadioButton) findViewById(R.id.radioButtonP);
+        rgStatus = (RadioGroup) findViewById(R.id.radioGroupStatus);
         buttonOK = (Button) findViewById(R.id.buttonOK);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                doClick();
+            }
+        });
+    }
+
+    private void doClick() {
     }
 }

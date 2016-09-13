37322f31edcecf8a524d3a0813d1b808f44d953e
Krisna Putra F <contact.krisnapf@gmail.com>
2016-09-13 09:17:19 +0700

    desain semua, belum di beri control, spinner belum terisi data

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2013/formpendaftaranclub/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2013/formpendaftaranclub/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -14,9 +15,10 @@ public class MainActivity extends AppCompatActivity {
     EditText etNama;
     CheckBox cbPG, cbSG, cbC;
     RadioGroup rgStatus;
+    RadioButton rbL, rbP;
     Button buttonOK;
     Spinner spKota;
-    TextView tvHasil;
+    TextView tvHasil, tvAskot, tvJeka, tvPS, tvNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +32,62 @@ public class MainActivity extends AppCompatActivity {
         rgStatus = (RadioGroup) findViewById(R.id.radioGroupStatus);
         buttonOK = (Button) findViewById(R.id.buttonOK);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
-        findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener() {
+        tvAskot = (TextView) findViewById(R.id.textViewAK);
+        tvNama = (TextView) findViewById(R.id.textViewNama);
+        tvPS = (TextView) findViewById(R.id.textViewPS);
+        tvJeka = (TextView) findViewById(R.id.textViewJeka);
+        rbP = (RadioButton) findViewById(R.id.radioButtonP);
+        rbL = (RadioButton) findViewById(R.id.radioButtonL);
+        buttonOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 doClick();
+                doProses();
+                doCoba();
             }
         });
     }
 
+    private void doCoba() {
+        String hasil = "Posisi Bermain : ";
+        int startlen = hasil.length();
+        if (cbPG.isChecked()) hasil += cbPG.getText() + "\n";
+        if (cbSG.isChecked()) hasil += cbSG.getText() + "\n";
+        if (cbC.isChecked()) hasil += cbC.getText() + "\n";
+        if (hasil.length() == startlen) hasil += "Belum Memilih Posisi";
+        tvPS.setText(hasil);
+    }
+
+    private void doProses() {
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+
+            tvNama.setText("Nama : " + nama);
+            tvAskot.setText("Asal Kota : " + spKota.getSelectedItem().toString());
+        }
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+        String nama = etNama.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama Belum di Isi");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+        return valid;
+    }
+
     private void doClick() {
+        String jeka = null;
+
+        if (rbL.isChecked()) {
+            jeka = rbL.getText().toString();
+        } else {
+            tvJeka.setText("Jenis Kelamin : " + jeka);
+        }
     }
 }
